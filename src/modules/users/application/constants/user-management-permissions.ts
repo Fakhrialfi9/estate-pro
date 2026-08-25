aaccbaa2abc66e3bd6521aab38a35b14afa307b3
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-25 22:46:07 +0700

    fix(users): centralize user CRUD permissions

---
+++ src/modules/users/application/constants/user-management-permissions.ts
@@ -0,0 +1,8 @@
+export const USER_MANAGEMENT_PERMISSIONS = {
+  GET: 'users.read',
+  HEAD: 'users.read',
+  POST: 'users.create',
+  PATCH: 'users.update',
+  PUT: 'users.update',
+  DELETE: 'users.delete',
+} as const;
