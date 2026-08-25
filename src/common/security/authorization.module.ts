c1bbb1c585e7ac48e20b8ca0ce13fdf858623c1c
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-25 23:13:54 +0700

    fix: centralize authorization provider wiring

---
+++ src/common/security/authorization.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { DatabaseModule } from '../../infrastructure/database/database.module.js';
+import { AuthorizationService } from './authorization.service.js';
+import { USER_AUTHORIZATION_REPOSITORY } from './authorization.repository.js';
+import { PrismaUserAuthorizationRepository } from '../../modules/roles/infrastructure/persistence/prisma-user-authorization.repository.js';
+
+@Global()
+@Module({
+  imports: [DatabaseModule],
+  providers: [
+    AuthorizationService,
+    {
+      provide: USER_AUTHORIZATION_REPOSITORY,
+      useClass: PrismaUserAuthorizationRepository,
+    },
+  ],
+  exports: [AuthorizationService, USER_AUTHORIZATION_REPOSITORY],
+})
+export class AuthorizationModule {}
