0f3fce62ffdb3711d199df561708d38d71b6f5e3
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-25 16:35:49 +0700

    refactor(authorization): isolate user role target lookup

---
+++ src/modules/roles/domain/repositories/user-role-target.repository.ts
@@ -0,0 +1,9 @@
+export interface UserRoleTarget {
+  uuid: string;
+}
+
+export interface UserRoleTargetRepository {
+  findByUuid(userUuid: string): Promise<UserRoleTarget | null>;
+}
+
+export const USER_ROLE_TARGET_REPOSITORY = Symbol('USER_ROLE_TARGET_REPOSITORY');
