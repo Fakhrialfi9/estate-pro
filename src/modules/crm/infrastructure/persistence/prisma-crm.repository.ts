40177ac229e24af652c94c73c6c0fbe040175daa
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-09-01 08:05:54 +0700

    Update

--- src/modules/crm/infrastructure/persistence/prisma-crm.repository.ts
+++ src/modules/crm/infrastructure/persistence/prisma-crm.repository.ts
@@ -554,10 +554,12 @@ export class PrismaCrmRepository implements CrmRepository {
         ...(i.campaignUuid !== undefined
           ? {
               campaignId: i.campaignUuid
-                ? (await this.prisma.crmLeadCampaign.findFirst({
-                    where: { uuid: toText(i.campaignUuid) },
-                    select: { id: true },
-                  }))?.id ?? null
+                ? ((
+                    await this.prisma.crmLeadCampaign.findFirst({
+                      where: { uuid: toText(i.campaignUuid) },
+                      select: { id: true },
+                    })
+                  )?.id ?? null)
                 : null,
             }
           : {}),
@@ -1397,9 +1399,11 @@ export class PrismaCrmRepository implements CrmRepository {
           ? ((await this.getActivity(toText(i.activityUuid))) as Row).id
           : null,
         templateId: i.templateUuid
-          ? (await this.prisma.crmCommunicationTemplate.findFirst({
-              where: { uuid: toText(i.templateUuid) },
-            }))?.id ?? null
+          ? ((
+              await this.prisma.crmCommunicationTemplate.findFirst({
+                where: { uuid: toText(i.templateUuid) },
+              })
+            )?.id ?? null)
           : null,
         providerName: i.providerName ? toText(i.providerName) : null,
         destination: toText(i.destination),
