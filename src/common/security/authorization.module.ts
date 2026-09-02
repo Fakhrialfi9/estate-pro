13c9d6399f6eab1222e914694d348b315d2293c0
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-09-03 02:54:38 +0700

    fix(authorization): export only locally owned providers

--- src/common/security/authorization.module.ts
+++ src/common/security/authorization.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../infrastructure/database/database.module.js';
 import { AuthorizationService } from './authorization.service.js';
-import { USER_AUTHORIZATION_REPOSITORY } from './authorization.repository.js';
 import { PropertyAccessGuard } from './property-access.guard.js';
 
 @Module({
   imports: [DatabaseModule],
   providers: [AuthorizationService, PropertyAccessGuard],
-  exports: [
-    AuthorizationService,
-    USER_AUTHORIZATION_REPOSITORY,
-    PropertyAccessGuard,
-  ],
+  exports: [AuthorizationService, PropertyAccessGuard],
 })
 export class AuthorizationModule {}
