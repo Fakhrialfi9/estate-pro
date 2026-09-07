924d6067eec8ad7f677e0a5d3330394c8177b8fb
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-09-07 22:02:18 +0700

    feat(property-matching): register matching rule services and repository

--- src/modules/property-matching/property-matching.module.ts
+++ src/modules/property-matching/property-matching.module.ts
@@ -6,6 +6,9 @@ import { AuthorizationModule } from '../../common/security/authorization.module.
 import { MatchingEngine } from './domain/matching-engine.js';
 import { MATCHING_REPOSITORY } from './application/matching.ports.js';
 import { PropertyMatchingService } from './application/property-matching.service.js';
+import { MatchingRuleService } from './application/matching-rule.service.js';
+import { MATCHING_RULE_REPOSITORY } from './domain/repositories/matching-rule.repository.js';
+import { PrismaMatchingRuleRepository } from './infrastructure/prisma-matching-rule.repository.js';
 import { PrismaPropertyMatchingRepository } from './infrastructure/prisma-property-matching.repository.js';
 import { PropertyMatchingController } from './presentation/property-matching.controller.js';
 
@@ -15,13 +18,20 @@ import { PropertyMatchingController } from './presentation/property-matching.con
   providers: [
     MatchingEngine,
     PropertyMatchingService,
+    MatchingRuleService,
     {
       provide: MATCHING_REPOSITORY,
       inject: [PrismaService],
       useFactory: (prisma: PrismaService) =>
         new PrismaPropertyMatchingRepository(prisma),
     },
+    {
+      provide: MATCHING_RULE_REPOSITORY,
+      inject: [PrismaService],
+      useFactory: (prisma: PrismaService) =>
+        new PrismaMatchingRuleRepository(prisma),
+    },
   ],
-  exports: [PropertyMatchingService],
+  exports: [PropertyMatchingService, MatchingRuleService],
 })
 export class PropertyMatchingModule {}
